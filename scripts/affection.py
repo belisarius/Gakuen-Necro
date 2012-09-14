d27ba8da71dfa0ae74f4144a9298a242eed376ce
Ben <squattingturnip@gmail.com>
2012-09-13 22:25:11 -0700

    Working on initial "calculate" function
    Have any idea of what to do, but too tired to think. :( Comming back to
    this tomorrow.

---
+++ scripts/affection.py
@@ -0,0 +1,24 @@
+# Gakuen Necro Early Development
+
+class Affection:
+    """A collection of scripts for dealing with boys and their affection for
+    you. <3"""
+    def calculate(attributes):
+        """Calculate attribute change, save, and return change value.
+
+        Specifically this recieves a comma-separated list of attributes,
+        with any applicable modifiers, iterates through that list and
+        builds a dictionary containing attribute:value pairs.
+        That dictionary is then evaluated to return the affection value
+        changes to the user and add + store the new affection values in
+        a persistent variable.
+
+        """
+        
+        for attribute in attributes:
+            tuple(attribute.split(':'))
+            
+            
+
+    
+    
