08afa66b10d588a3afc2f9c3ebd93f91b24c893d
Ben <squattingturnip@gmail.com>
2012-09-14 23:39:19 -0700

    Initial evaluation logic complete
    I've removed the class definition for now, since I'm not entirely sure
    how to work with it. For now, passing a variable containing the list of
    attributes and multiplier values results in the correct response—a list
    containing tuples of attribute/multiplier pairs. First stage done!

--- scripts/affection.py
+++ scripts/affection.py
@@ -1,24 +1,40 @@
 # Gakuen Necro Early Development
 
-class Affection:
-    """A collection of scripts for dealing with boys and their affection for
-    you. <3"""
-    def calculate(attributes):
-        """Calculate attribute change, save, and return change value.
+# class Affection:
+    # """A collection of scripts for dealing with boys and their affection for
+    # you. <3"""
+def calculate(attributes):
+    """Calculate attribute change, save, and return change value.
 
-        Specifically this recieves a comma-separated list of attributes,
-        with any applicable modifiers, iterates through that list and
-        builds a dictionary containing attribute:value pairs.
-        That dictionary is then evaluated to return the affection value
-        changes to the user and add + store the new affection values in
-        a persistent variable.
+    Specifically this recieves a comma-separated list of attributes,
+    with any applicable modifiers, iterates through that list and
+    builds a dictionary containing attribute:value pairs.
+    That dictionary is then evaluated to return the affection value
+    changes to the user and add + store the new affection values in
+    a persistent variable.
 
-        """
-        
-        for attribute in attributes:
-            tuple(attribute.split(':'))
-            
-            
+    """
+    # Create the attr list.
+    attr = []
 
-    
-    
+    # Iterate through all items that have been passed to the fucntion.
+    for a in attributes:
+        # If the attribute includes a :--that is, it has a modifier--then we
+        # need to do some special stuff with it.
+        if ':' in a:
+            # Split at colon, then convert to tuple.
+            a = tuple(a.split(':'))
+            # Convert the second item from a string to an integer.
+            # If we don't things will get ugly.
+            a = a[0],int(a[1])
+            # At this point, 'a' should be a tuple in the form of
+            # ('attribute',n).
+        else:
+            # If the attribute doesn't have a modifier, then a a "modifier"
+            # of 1.
+            a = a,1
+            # At this point, 'a' should be a tuple in the form of
+            # ('attribute',n).
+        # Append the 'a' tuple to the list 'attr'.
+        attr.append(a)
+    return attr
